a8e143f5681ede8fa63ce986239b38f9c3e934b3
chenshuai <chenshuai@ian2018.cn>
2023-04-02 18:10:37 +0800

    change error

--- src/components/common/Setting/General.vue
+++ src/components/common/Setting/General.vue
@@ -1,17 +1,19 @@
 <script lang="ts" setup>
-import { computed, ref } from 'vue'
+import { computed } from 'vue'
+// import { computed, ref } from 'vue'
 import { NButton, NPopconfirm, NSelect, useMessage } from 'naive-ui'
 // import { NButton, NInput, NPopconfirm, NSelect, useMessage } from 'naive-ui'
 import type { Language, Theme } from '@/store/modules/app/helper'
 import { SvgIcon } from '@/components/common'
-import { useAppStore, useUserStore } from '@/store'
+import { useAppStore } from '@/store'
+// import { useAppStore, useUserStore } from '@/store'
 // import type { UserInfo } from '@/store/modules/user/helper'
 import { getCurrentDate } from '@/utils/functions'
 import { useBasicLayout } from '@/hooks/useBasicLayout'
 import { t } from '@/locales'
 
 const appStore = useAppStore()
-const userStore = useUserStore()
+// const userStore = useUserStore()
 
 const { isMobile } = useBasicLayout()
 

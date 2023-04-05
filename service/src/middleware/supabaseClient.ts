2f04c1ff959e045022ea963353779695d8c63795
chenshuai <chenshuai@ian2018.cn>
2023-04-05 18:49:53 +0800

    修改密钥缓存问题

--- service/src/middleware/supabaseClient.ts
+++ service/src/middleware/supabaseClient.ts
@@ -4,7 +4,7 @@ import cache from 'memory-cache'
 const supabase = createClient('https://yvargccllzgzjbkoypba.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl2YXJnY2NsbHpnempia295cGJhIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODA2NjUxMjksImV4cCI6MTk5NjI0MTEyOX0.s--34SjoqnxWh4kmC0s16UbHADMP_vWIHlEs9gVmsjM')
 
 export async function getUserPass(pass: string, force = false) {
-  const cachePass = cache.get('pass')
+  const cachePass = cache.get(pass)
   if (cachePass && !force) {
     global.console.log('user cache pass')
     return JSON.parse(cachePass)
@@ -12,6 +12,6 @@ export async function getUserPass(pass: string, force = false) {
   const { data } = await supabase.from('user_pass_key').select().eq('pass', pass)
   const userPass = data[0]
   if (userPass)
-    cache.put('pass', JSON.stringify(userPass), 60 * 60 * 24 * 1 * 1000)
+    cache.put(pass, JSON.stringify(userPass), 60 * 60 * 12 * 1000)
   return userPass
 }
